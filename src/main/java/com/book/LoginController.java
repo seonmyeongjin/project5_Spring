111c45a5c88b559aaf1d1492ac5105894c6a5d48
seol-wonjun <qiops@naver.com>
2023-12-10 06:06:07 +0900

    edit

--- src/main/java/com/book/LoginController.java
+++ src/main/java/com/book/LoginController.java
@@ -14,6 +14,7 @@ public class LoginController {
     UserDAO userDAO;
     @RequestMapping(value = "/login", method = RequestMethod.GET)
     public String login() {
+        //하이루
         return"login";
     }
 
